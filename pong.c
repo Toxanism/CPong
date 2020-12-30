5c17fac92e34b5d7a70e5e522674cc8468a42eb9
TheCasProg <68574716+TheCasProg@users.noreply.github.com>
2020-12-30 12:23:39 -0800

    Add files via upload

--- pong.c
+++ pong.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define bool int
+#define true 1
+#define false 2
+
+int draw(int X_border, int Y_border);
+
 int main()
 {
+  bool Is_alive = true;
+  while(Is_alive != false){
+    draw(40, 40);
+    Is_alive = false;
+  }
   return 0;
+}
+
+int draw(int X_border, int Y_border){
+  
+  for (int i = 1; i < X_border + 1; i++){
+    printf("# ");
+  }
+  printf("\n");
+  
+  for (int i = 1; i < Y_border + 1; i++){
+    for (int j = 1; j < X_border + 1; j++){
+      if (j == 1 || j == X_border || j == X_border/2){
+	printf("# ");
+      }
+      else{
+	printf("  ");
+      }
+    }
+    printf("\n");
+  }
+
+  for (int i = 1; i < X_border + 1; i++){
+    printf("# ");
+  }
+  printf("\n");
+  
 }
