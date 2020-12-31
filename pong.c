23278056d563b534a3f021d763093ee64b7b1fe3
TheCasProg <68574716+TheCasProg@users.noreply.github.com>
2020-12-30 19:02:57 -0800

    Add files via upload

--- pong.c
+++ pong.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define bool int
 #define true 1
-#define false 2
+#define false 0
 
-int draw(int X_border, int Y_border);
+void draw(int X_border, int Y_border);
+
+const int X_player1 = 2;
+int Y_player1 = 20;
+
+const int X_player2 = 39;
+int Y_player2 = 20;
 
 int main()
 {
@@ -17,17 +22,18 @@ int main()
   return 0;
 }
 
-int draw(int X_border, int Y_border){
-  
+void draw(int X_border, int Y_border){
   for (int i = 1; i < X_border + 1; i++){
     printf("# ");
   }
   printf("\n");
-  
-  for (int i = 1; i < Y_border + 1; i++){
-    for (int j = 1; j < X_border + 1; j++){
-      if (j == 1 || j == X_border || j == X_border/2){
+  for (int y = 1; y < Y_border + 1; y++){
+    for (int x = 1; x < X_border + 1; x++){
+      if (x == 1 || x == X_border || x == X_border/2){
 	printf("# ");
+      }
+      else if ((x == X_player1 && y == Y_player1) || (x == X_player2 && y == Y_player2)){
+	printf("||");
       }
       else{
 	printf("  ");
@@ -35,10 +41,8 @@ int draw(int X_border, int Y_border){
     }
     printf("\n");
   }
-
   for (int i = 1; i < X_border + 1; i++){
     printf("# ");
   }
   printf("\n");
-  
 }
